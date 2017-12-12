db41cb50f9b9a38c5c93b620dd12eebad5b38d1f
MrsPepa <jose.leon1996@gmail.com>
2017-12-12 10:31:04 -0300

    actualizando js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+
+    $('.left').click(function(){
+    	console.log('holito');
+    	var name = $('#name').val();
+    	var cell = $('#cellphone').val();
+    	
+    })
   });
