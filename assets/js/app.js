afc46934aa8c99d2b4f88cc6e4978d4d1ec933d0
MrsPepa <jose.leon1996@gmail.com>
2017-12-18 19:41:00 -0300

    actualizando agenda, eliminar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,27 @@
+var contact = '';
 $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $('.modal-tirgger').modal();
+
+    $('.left').click(function(){
+    	var name = $('#icon_prefix').val();
+      var phone = $('#icon_telephone').val();
+
+      $('#agregar').append(("<div id=contact" + contact + "' class='contact col s12'>'" +
+        "<div class='card horizontal'>" +
+        "<div class='card-image'>" +
+        "</div" +
+        "<div class='card-stacked'>" +
+        "<div class='card-content'>" +
+        "<h4 id='info'>" + '<i class="material-icons">account_circle</i>' + ' ' + name + "</h4>" +
+        '<p id="number">' + '<i class="material-icons">local_phone</i>' + ' ' + phone + '</p>' +
+        '<a class="waves-effect waves-light btn trash right" id="trash"><i class="material-icons">delete</i></a>' +
+        "</div>" +
+        "</div>" +
+        "</div>"))
+      $('.trash').click(function(){
+        $(this).parent().remove();
+      })
+    })
   });
